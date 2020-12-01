d43aa288ae82b944c0dd0a172a2f059fedadec87
wangsc <wsc4007@gmail.com>
2020-12-01 17:04:18 +0800

    数字类

--- src/main/com/number/TestNumber.java
+++ src/main/com/number/TestNumber.java
@@ -1,4 +1,38 @@
 package main.com.number;
 
+/**
+ * 数字学习
+ */
 public class TestNumber {
+
+
+    public static void main(String[] args) {
+
+//    数字转字符串
+//      int i = 5;
+//      //方法1
+//      String str = String.valueOf(i);
+//      //方法2
+//      Integer it = i;
+//      String str2 = it.toString();
+
+//    字符串转数字顶
+
+//        String str = "999";
+//        int i= Integer.parseInt(str); // 重载方法Integer.parseInt(str,10); 第二个参数表示转换为几进制
+//        System.out.println(i);
+
+//        把浮点数 3.14 转换为 字符串 "3.14"
+//        再把字符串 “3.14” 转换为 浮点数 3.14
+//        如果字符串是 3.1a4，转换为浮点数会得到什么？
+//        如果使用数字类的pares...进行转换会报错 -- 数字转换异常
+
+//        double d = 3.14;
+//        String s = String.valueOf(d); //double转换为字符串
+//        float f = Float.valueOf(s); //字符串转换为float
+//        float f = Float.parseFloat("3.1a4"); //字符串转换为float
+//        Integer i = Integer.parseInt("a314"); //字符a串转换为float
+
+
+    }
 }
