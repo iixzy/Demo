5bc24e3092a8cb727c280ebca88b45ae1e8c5f39
wangsc <wsc4007@gmail.com>
2020-12-01 17:06:57 +0800

    字符串构造器实现

---
+++ src/main/com/string/MyStringBuffer.java
@@ -0,0 +1,47 @@
+package main.com.string;
+
+/**
+ * 实现StringBuffer的字符构造器
+ */
+public class MyStringBuffer implements IStringBuffer{
+
+    @Override
+    public void append(String str) {
+
+    }
+
+    @Override
+    public void append(char c) {
+
+    }
+
+    @Override
+    public void insert(int pos, char b) {
+
+    }
+
+    @Override
+    public void insert(int pos, String b) {
+
+    }
+
+    @Override
+    public void delete(int start) {
+
+    }
+
+    @Override
+    public void delete(int start, int end) {
+
+    }
+
+    @Override
+    public void reverse() {
+
+    }
+
+    @Override
+    public int length() {
+        return 0;
+    }
+}
